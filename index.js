cfef990c60b8727ce1a1c20b3ff3821097faef4a
woongkiki <kwk4348@nate.com>
2022-11-21 11:55:41 +0900

    index commit 1121

--- index.js
+++ index.js
@@ -2,8 +2,14 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import {AppRegistry, Text, TextInput, Platform} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+
+Text.defaultProps = Text.defaultProps || {};
+Text.defaultProps.allowFontScaling = false;
+
+TextInput.defaultProps = TextInput.defaultProps || {};
+TextInput.defaultProps.allowFontScaling = false;
 
 AppRegistry.registerComponent(appName, () => App);
